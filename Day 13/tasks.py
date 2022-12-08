e6e0af073081b893cd2433766ce71f71cd211460
Daniel Bara <29439900+danielbara00@users.noreply.github.com>
2022-12-08 22:34:08 +0000

    Scaffolding

---
+++ Day 13/tasks.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+def main():
+    with open("Day 13/input.txt") as file:
+        lines = file.readlines()
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        with open("Day 13/input_test.txt") as file:
+            self.lines_test = file.readlines()
+
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
+    main()
