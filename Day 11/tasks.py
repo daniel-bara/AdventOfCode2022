c1355e9bb06af4734e22c4edc624966f0cd8e8ca
Daniel Bara <daniel.bara@hq.trayport.com>
2022-12-24 17:12:43 +0000

    Solve days 11 and 12

--- Day 11/tasks.py
+++ Day 11/tasks.py
@@ -1,18 +1,99 @@
 import unittest
+import math
 
 
 def main():
     with open("Day 11/input.txt") as file:
-        lines = list(map(str.strip, file.readlines()))
+        input=file.read()
+    print("Task 1:", task1(input))
+    print("Task 2:", task2(input))
+
+
+def task1(monkeys_input):
+    monkey_list = list(map(Monkey1, monkeys_input.split("\n\n")))
+    for i in range(20):
+        for monkey in monkey_list:
+            for item in monkey.process_items():
+                monkey_list[item["destination"]].receive_item(item)
+    return math.prod(sorted(map(lambda monkey: monkey.inspection_count,monkey_list), reverse=True)[:2])
+
+
+def task2(monkeys_input, test = False):
+    monkey_list = list(map(lambda line:Monkey2(line, test), monkeys_input.split("\n\n")))
+    for i in range(10000):
+        for monkey in monkey_list:
+            # print("Monkey", monkey.number, "with items", monkey.items)
+            for item in monkey.process_items():
+                monkey_list[item["destination"]].receive_item(item)
+                # print("Item ", item["worry_level"], "to", item["destination"])
+    print(list(map(lambda monkey: monkey.inspection_count,monkey_list)))
+    return math.prod(sorted(map(lambda monkey: monkey.inspection_count,monkey_list), reverse=True)[:2])
+
+class Monkey:
+    def __init__(self, init_string, test=False):
+        self.test = test
+        lines = init_string.splitlines()
+        self.number = lines[0].split(" ")[1].split(":")[0]
+        self.items = list(map(int, lines[1].split(": ")[1].split(", ")))
+        self.operation = lines[2].split("new = ")[1]
+        self.divisor = int(lines[3].split(" ")[-1])
+        self.test_true_monkey = int(lines[4].split(" ")[-1])
+        self.test_false_monkey = int(lines[5].split(" ")[-1])
+        self.inspection_count = 0
+
+    def receive_item(self, item):
+        self.items.append(item["worry_level"])
+
+    def worry_level_decrease(self, old_level):
+        raise NotImplementedError()
+
+    def process_items(self):
+        for _ in range(len(self.items)):
+            self.inspection_count += 1
+            old = self.items.pop(0)
+            new = eval(self.operation)
+            new = self.worry_level_decrease(new)
+            if new % self.divisor == 0:
+                yield {"worry_level": new, "destination": self.test_true_monkey}
+            else:
+                yield {"worry_level": new, "destination": self.test_false_monkey}
+
+class Monkey1(Monkey):
+    def worry_level_decrease(self, old_level):
+        return int(old_level / 3)
+
+class Monkey2(Monkey):
+    def worry_level_decrease(self, old_level):
+        divisor_product = (13*17*19*23) if self.test else (2*3*5*7*11*13*17*19)
+        if old_level > divisor_product:
+            return old_level % divisor_product
+        return old_level
+        
 
 
 class Tests(unittest.TestCase):
     def setUp(self):
         with open("Day 11/input_test.txt") as file:
-            self.lines_test = list(map(str.strip, file.readlines()))
+            self.test = file.read()
 
-    def test(self):
-        pass
+    def test_process_items(self):
+        sut = Monkey1(
+            '''Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3''')
+        items_returned = list(sut.process_items())
+        self.assertEqual(items_returned, [
+            {"worry_level": 500, "destination": 3},
+            {"worry_level": 620, "destination": 3}])
+    
+    def test_task1(self):
+        self.assertEqual(10605, task1(self.test))
+    
+    def test_task2(self):
+        self.assertEqual(2713310158, task2(self.test, test=True))
 
 
 if __name__ == "__main__":
--- Day 12/tasks.py
+++ Day 12/tasks.py
@@ -1,9 +1,101 @@
 import unittest
+import math
 
 
 def main():
     with open("Day 12/input.txt") as file:
         lines = list(map(str.strip, file.readlines()))
+    print("Task 1:", best_first(lines))
+    print("Task 2:", reverse_best_first(lines))
+
+
+def make_tile_matrix(input_matrix, reverse=False):
+    def can_climb(tile, neighbor):
+        if reverse:
+            return tile.elevation - neighbor.elevation <= 1
+        return tile.elevation - neighbor.elevation >= -1
+    tile_matrix = [list(map(Tile, row)) for row in input_matrix]
+    for outer_y in range(len(input_matrix)):
+        for outer_x in range(len(input_matrix[0])):
+            for (x, y) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                tile = tile_matrix[outer_y][outer_x]
+                tile.x = outer_x
+                tile.y = outer_y
+                try:
+                    neighbor = tile_matrix[outer_y + y][outer_x + x]
+                    if (outer_x + x) in range(len(tile_matrix[0])) and (outer_y + y) in range(len(tile_matrix)) and\
+                            can_climb(tile, neighbor):
+                        tile.possible_next.append(neighbor)
+                except:
+                    pass
+    return tile_matrix
+
+
+def find_start(tile_matrix):
+    for y in range(len(tile_matrix)):
+        for x in range(len(tile_matrix[0])):
+            if tile_matrix[y][x].type == "start":
+                return x, y
+
+def find_E(tile_matrix):
+    for y in range(len(tile_matrix)):
+        for x in range(len(tile_matrix[0])):
+            if tile_matrix[y][x].type == "end":
+                return x, y
+
+
+def best_first(input_matrix):
+    tile_matrix = make_tile_matrix(input_matrix)
+    start = find_start(tile_matrix)
+    start_tile = tile_matrix[start[1]][start[0]]
+    backlog = [(start_tile, 0)]
+    tile = start_tile
+    while tile.type != "end":
+        backlog.sort(key=lambda i: i[1])
+        tile, cost = backlog.pop(0)
+        for next_state in explore(tile, cost):
+            backlog.append(next_state)
+    return tile.cost
+
+
+def reverse_best_first(input_matrix):
+    tile_matrix = make_tile_matrix(input_matrix, reverse=True)
+    start = find_E(tile_matrix)
+    start_tile = tile_matrix[start[1]][start[0]]
+    backlog = [(start_tile, 0)]
+    tile = start_tile
+    while tile.elevation != ord('a'):
+        backlog.sort(key=lambda i: i[1])
+        tile, cost = backlog.pop(0)
+        for next_state in explore(tile, cost):
+            backlog.append(next_state)
+    return tile.cost
+
+
+def explore(tile, cost):
+    # print(tile.x, tile.y, tile.letter, cost+1)
+    for neighbor in tile.possible_next:
+        if cost + 1 < neighbor.cost:
+            neighbor.cost = cost + 1
+            yield neighbor, neighbor.cost
+
+
+class Tile:
+    def __init__(self, letter):
+        self.letter = letter
+        self.x = -1
+        self.y = -1
+        if letter == "S":
+            self.type = "start"
+            self.elevation = ord("a")
+        elif letter == "E":
+            self.type = "end"
+            self.elevation = ord("z")
+        else:
+            self.type = "normal"
+            self.elevation = ord(letter)
+        self.possible_next = []
+        self.cost = math.inf
 
 
 class Tests(unittest.TestCase):
@@ -11,8 +103,23 @@ class Tests(unittest.TestCase):
         with open("Day 12/input_test.txt") as file:
             self.lines_test = list(map(str.strip, file.readlines()))
 
-    def test(self):
-        pass
+    def test_make_state_map(self):
+        sut = make_tile_matrix(self.lines_test)
+        self.assertEqual(['a', 'a', 'c', 'c'], list(
+            sorted(map(lambda tile: tile.letter, sut[1][1].possible_next))))
+        self.assertEqual(['c', 'e', 's', 'u'], list(
+            sorted(map(lambda tile: tile.letter, sut[3][3].possible_next))))
+        self.assertEqual(['a', 'c'], list(
+            sorted(map(lambda tile: tile.letter, sut[0][2].possible_next))))
+
+    def test_best_first(self):
+        self.assertEqual(25, best_first(["SbcdefghijklmnopqrstuvwxyE"]))
+        self.assertEqual(26, best_first([
+            "Sbcdefghijklmnopqrstuvwxya",
+            "zbcdefghijklmnopqrstuvwxyE"]))
+        self.assertEqual(31, best_first(self.lines_test))
+    def test_reverse_best_first(self):
+        self.assertEqual(29, reverse_best_first(self.lines_test))
 
 
 if __name__ == "__main__":
